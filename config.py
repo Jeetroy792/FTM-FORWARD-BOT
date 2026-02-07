fe7128814ac00010361c2365830b2eb8a3dd708e
Jeetroy792 <jeetroy5335@gmail.com>
2026-02-07 11:48:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,11 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "22141398")
-    API_HASH = os.environ.get("API_HASH", "0c8f8bd171e05e42d6f6e5a6f4305389")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8088891636:AAH1BwhfzWcLo77Qct7wvPBNBn71RErh5TE") 
+    API_ID = os.environ.get("API_ID", "24670806")
+    API_HASH = os.environ.get("API_HASH", "82134723a32b2cae76b9cfb3b1570745")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8596004805:AAEb22RfZBfXYlvXa2FLVhtKBcBsIXSzi4o") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://mrindianlekhak:ftm@cluster0.3ruvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Admin:golu1234@cluster0.bcz3n2q.mongodb.net/?appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "Cluster0")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '8122637721').split()]
 
